03bc487a7e883bbf3c1fa5304fa8ac6fe81867e7
PauApps <appspauapps@gmail.com>
2019-03-20 11:42:25 +0100

    1.3.1

--- app/src/main/java/com/pauapps/pau/allrandomizer/MainActivity.java
+++ app/src/main/java/com/pauapps/pau/allrandomizer/MainActivity.java
@@ -151,7 +151,8 @@ public class MainActivity extends Activity {
                     intent.putExtra(Intent.EXTRA_TEXT,
                             "Hey!\nI make a random process of this items: \n" + items +
                                     "\n \nAnd random result is ... \n" + res + "\nMake you own lists on " +
-                                    "http://bit.ly/AllRandomizer");
+                                    "http://bit.ly/AllRandomizer \n\n " +
+                                    "Follow us on Twitter: https://twitter.com/pauapps");
                     startActivity(Intent.createChooser(intent, "Share with"));
                 }
             });
