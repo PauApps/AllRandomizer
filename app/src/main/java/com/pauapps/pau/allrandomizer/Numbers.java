16cba9d2e75ff2664ddd2784975696fdccb8d774
PauApps <appspauapps@gmail.com>
2019-03-25 15:41:00 +0100

    1.4

--- app/src/main/java/com/pauapps/pau/allrandomizer/Numbers.java
+++ app/src/main/java/com/pauapps/pau/allrandomizer/Numbers.java
@@ -1,4 +1,31 @@
 package com.pauapps.pau.allrandomizer;
 
-public class Numbers {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Numbers extends Activity {
+
+    EditText number;
+    int num;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.numbers);
+
+        number = (EditText) findViewById(R.id.number);
+    }
+
+    public void sum(View v) {
+        num = Integer.parseInt(number.getText().toString());
+        num++;
+        number.setText(num);
+    }
+
+    public void res(View v) {
+        num = Integer.parseInt(number.getText().toString());
+        num--;
+        number.setText(num);
+    }
 }
